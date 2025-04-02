3eb6e3f7bfbe25e8a80c98dc62583bfe846e2ef3
Altinai333 <atoucheteau@putnamscience.org>
2025-04-02 09:20:13 -0400

    I code the most important
    i will start my loop

--- emil_project.py
+++ emil_project.py
@@ -1,2 +1,22 @@
-print ("hello world")
- 
+import random
+def choose_word():
+ words = {"chromokopia", "Igor", "wolf"}
+ return random.choice(words)
+
+def display_word(word,guessed_letters):
+    display = ""
+    for letters in the word:
+        if letter guessed_letters:
+        else:
+           display+= "_"
+    return display
+def hangman ():
+   max_attempts = 6 guessed_letters = {}
+   word_to_guess = choose_word()
+   attempts = 0 
+
+print("welcome to hangman")
+print(display_word(word_to_guess, guessed_letters))
+
+while true:
+   
