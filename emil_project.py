d924106148ad2a67af88c6928776cbfa9efa8ee3
Altinai333 <atoucheteau@putnamscience.org>
2025-04-07 09:01:49 -0400

    i make a couple quotes
    quotes

--- emil_project.py
+++ emil_project.py
@@ -5,18 +5,37 @@ def choose_word():
 
 def display_word(word,guessed_letters):
     display = ""
-    for letters in the word:
+    for letters in word:
         if letter guessed_letters:
         else:
            display+= "_"
     return display
 def hangman ():
-   max_attempts = 6 guessed_letters = {}
+   max_attempts = 6 
+   guessed_letters = {}
    word_to_guess = choose_word()
    attempts = 0 
+
 
 print("welcome to hangman")
 print(display_word(word_to_guess, guessed_letters))
 
 while true:
-   
+if max_attempts > 6:
+   break
+
+guess = input ("guess a letter").lower()
+if guess.isalpha() and len () guess == 1: 
+elif guess in guessed_letters:
+  guessed_letters.append (guess)
+  print ("good guess!")
+else:
+ print ("incorrect guess, attempts remaining:"), max_attempts - attempts 
+
+print ("invalid input, please enter a single letter")
+ print  (display_word(word_to_guess, guessed_letters))
+
+if set(guessed_letters == set (word_to_guess):
+       print  ("congratulations you guessed the firt word")
+if attempts == max_attempts:
+print ("sorry you run out of attempts, the word was:")
