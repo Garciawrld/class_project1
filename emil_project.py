7cfdf9483796a9dcfd3984a2897c716a9cce32dd
Altinai333 <atoucheteau@putnamscience.org>
2025-04-08 09:12:40 -0400

    i move my loop
    i move my loop

--- emil_project.py
+++ emil_project.py
@@ -7,8 +7,9 @@ def display_word(word,guessed_letters):
     display = ""
     for letters in word:
         if letter guessed_letters:
-        else:
-           display+= "_"
+      
+       else:
+          display+= "_"
     return display
 def hangman ():
    max_attempts = 6 
@@ -19,23 +20,21 @@ def hangman ():
 
 print("welcome to hangman")
 print(display_word(word_to_guess, guessed_letters))
-
-while true:
-if max_attempts > 6:
-   break
-
 guess = input ("guess a letter").lower()
 if guess.isalpha() and len () guess == 1: 
-elif guess in guessed_letters:
-  guessed_letters.append (guess)
-  print ("good guess!")
+      elif guess in guessed_letters:
+   guessed_letters.append (guess)
+    print ("good guess!")
 else:
- print ("incorrect guess, attempts remaining:"), max_attempts - attempts 
+   print ("incorrect guess, attempts remaining:"), max_attempts - attempts 
 
 print ("invalid input, please enter a single letter")
- print  (display_word(word_to_guess, guessed_letters))
+print  (display_word(word_to_guess, guessed_letters))
 
 if set(guessed_letters == set (word_to_guess):
        print  ("congratulations you guessed the firt word")
-if attempts == max_attempts:
-print ("sorry you run out of attempts, the word was:")
+   if attempts == max_attempts:
+   print ("sorry you run out of attempts, the word was:")
+while true:
+   if max_attempts > 6:
+      break
